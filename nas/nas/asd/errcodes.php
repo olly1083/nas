86657c3ddb6adf1823d38edb4c34cc46cf387d6a
olly1083 <142718035+olly1083@users.noreply.github.com>
2024-05-25 07:46:38 +0100

    Upload errcodes.php

---
+++ nas/nas/asd/errcodes.php
@@ -0,0 +1,33 @@
+<?php
+
+    /**
+     * All the posibles errcodes returned by the server
+     * 
+     * -4 - Some fields missing
+     *
+     * -404 - DB Error
+     * -405 - Second query DB error
+     * 
+     * 3 - You're not logged in
+     * 2 - You're banned
+     * 1 - The task todo was made properly
+     * -1 - The tast todo wasn't made properly
+     * 0 - Wrong secret
+     * -2 - No secret was passed
+     * -3 - Passed the incorrect arguments number
+     * 
+     * -180 - Passwords doesn't match
+     * -181 - This username already exists
+     * -182 - The password can't be empty
+     * -183 - The username can't be empty
+     * 
+     * -90 This username doesn't exists
+     * -91 Wrong password
+     * -92 The username can't be empty
+     * -93 The password can't be empty
+     *
+     * -81 No id was passed
+     * -82 This level doesn't exist
+     */
+
+?>
